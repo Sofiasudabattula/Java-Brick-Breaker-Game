2dae9c8d6f834049d8e99d12407dbb6364653708
Sofia Sudabattula <sofiasudabattula929@gmail.com>
2025-09-24 12:57:29 +0530

    Initial commit of Brick Breaker project

---
+++ Brick Breaker Game/src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import javax.swing.JFrame;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        JFrame obj=new JFrame();
+        Gameplay gameplay=new Gameplay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+
+    }
+}
